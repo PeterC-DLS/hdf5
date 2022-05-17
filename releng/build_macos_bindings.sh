b75f1ce0722df5b068301bbef28f4908247fb1ca
Peter Chang <peter.chang@diamond.ac.uk>
2022-05-17 10:54:27 +0100

    Add new actions to build java bindings
    Add supporting scripts for Linux, macOS and win32 systems and on x86_64 and
    AArch64 architectures

---
+++ releng/build_macos_bindings.sh
@@ -0,0 +1,23 @@
+
+export JDKDIR=$JAVA_HOME_11_X64
+
+export PLAT_OS=macos
+export ARCH=x86_64
+
+./releng/build_java_bindings.sh
+X86_DEST=/io/dist/*/$PLAT_OS/$ARCH
+
+
+export ARCH=aarch64
+DONT_TEST_PLUGINS=yes ./releng/build_java_bindings.sh
+AA64_DEST=$(realpath -L $X86_DEST/../$ARCH)
+
+# Create universal2 versions
+UNI2_DEST=$(realpath -L $X86_DEST/../universal2)
+mkdir -p $UNI_DEST
+for l in $AA64_DEST/*.dylib; do
+    dlib=$(basename $l)
+    lipo -create $l $X86_DEST/$dlib -output $UNI2_DEST/$dlib
+done
+
+
